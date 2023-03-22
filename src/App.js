9b60e3f9e2322129d4d3bb994beabc25d52801e8
farizz <fairizwalker@gmail.com>
2023-03-23 02:38:04 +0700

    akhirnyooo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TESTING AJAH</h1>
+      <div className="video">
+        <iframe
+          title="no"
+          src="https://g3.ipcamlive.com/player/player.php?alias=641b4c088bb4e"
+          frameBorder={"none"}
+          allowFullScreen={true}
+        />
+        <iframe
+          title="no"
+          src="https://g3.ipcamlive.com/player/player.php?alias=641b4e17e90ea"
+          frameBorder={"none"}
+          allowFullScreen={true}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
